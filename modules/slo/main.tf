cedc05abbfec65bb222f07534f3c512b450afcc8
Steven Nemetz <snemetz@notablehealth.com>
2023-04-20 18:10:45 -0700

    Fleshing out SLO

--- modules/slo/main.tf
+++ modules/slo/main.tf
@@ -25,15 +25,44 @@ resource "google_monitoring_slo" "appeng_slo" {
       location = basic_sli.value.location
       method   = basic_sli.value.method
       version  = basic_sli.value.version
-      #dynamic "availability" {}
-      #dynamic "latency" {}
+      dynamic "availability" {
+        for_each = basic_sli.value.availability
+        content {
+          enabled = availability.value.enabled
+        }
+      }
+      dynamic "latency" {
+        for_each = basic_sli.value.latency
+        content {
+          threshold = latency.value.threshold
+        }
+      }
     }
   }
   dynamic "request_based_sli" {
     for_each = each.value.request_based_sli
     content {
-      #dynamic "distribution_cut" {}
-      #dynamic "good_total_ratio" {}
+      dynamic "distribution_cut" {
+        for_each = request_based_sli.value.distribution_cut
+        content {
+          distribution_filter = distribution_cut.value.distribution_filter
+          dynamic "range" {
+            for_each = distribution_cut.value.range
+            content {
+              min = range.value.min
+              max = range.value.max
+            }
+          }
+        }
+      }
+      dynamic "good_total_ratio" {
+        for_each = request_based_sli.value.good_total_ratio
+        content {
+          bad_service_filter   = good_total_ratio.value.bad_service_filter
+          good_service_filter  = good_total_ratio.value.good_service_filter
+          total_service_filter = good_total_ratio.value.total_service_filter
+        }
+      }
     }
   }
   dynamic "windows_based_sli" {
@@ -41,9 +70,84 @@ resource "google_monitoring_slo" "appeng_slo" {
     content {
       good_bad_metric_filter = windows_based_sli.value.good_bad_metric_filter
       window_period          = windows_based_sli.value.window_period
-      #dynamic "good_total_ratio_threshold" { }
-      #dynamic "metric_mean_in_range" {}
-      #dynamic "metric_sum_in_range" {}
+      dynamic "good_total_ratio_threshold" {
+        for_each = windows_based_sli.value.good_total_ratio_threshold
+        content {
+          threshold = good_total_ratio_threshold.value.threshold
+          dynamic "basic_sli_performance" {
+            for_each = good_total_ratio_threshold.value.basic_sli_performance
+            content {
+              method   = basic_sli_performance.value.method
+              location = basic_sli_performance.value.location
+              version  = basic_sli_performance.value.version
+              dynamic "availability" {
+                for_each = basic_sli_performance.value.availability
+                content {
+                  enabled = availability.value.enabled
+                }
+              }
+              dynamic "latency" {
+                for_each = basic_sli_performance.value.latency
+                content {
+                  threshold = latency.value.threshold
+                }
+              }
+            }
+          }
+          dynamic "performance" {
+            for_each = good_total_ratio_threshold.value.performance
+            content {
+              dynamic "distribution_cut" {
+                for_each = performance.value.distribution_cut
+                content {
+                  distribution_filter = distribution_cut.value.distribution_filter
+                  dynamic "range" {
+                    for_each = distribution_cut.value.range
+                    content {
+                      max = range.value.max
+                      min = range.value.min
+                    }
+                  }
+                }
+              }
+              dynamic "good_total_ratio" {
+                for_each = performance.value.good_total_ratio
+                content {
+                  bad_service_filter   = good_total_ratio.value.bad_service_filter
+                  good_service_filter  = good_total_ratio.value.good_service_filter
+                  total_service_filter = good_total_ratio.value.total_service_filter
+                }
+              }
+            }
+          }
+        }
+      }
+      dynamic "metric_mean_in_range" {
+        for_each = windows_based_sli.value.metric_mean_in_range
+        content {
+          time_series = metric_mean_in_range.value.time_series
+          dynamic "range" {
+            for_each = metric_mean_in_range.value.range
+            content {
+              max = range.value.max
+              min = range.value.min
+            }
+          }
+        }
+      }
+      dynamic "metric_sum_in_range" {
+        for_each = windows_based_sli.value.metric_sum_in_range
+        content {
+          time_series = metric_sum_in_range.value.time_series
+          dynamic "range" {
+            for_each = metric_sum_in_range.value.range
+            content {
+              max = range.value.max
+              min = range.value.min
+            }
+          }
+        }
+      }
     }
   }
 }
