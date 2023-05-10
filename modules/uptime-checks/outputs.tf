651cf691ab162f6e84e35dba33abfbecc9cfa8dd
Steven Nemetz <snemetz@notablehealth.com>
2023-05-10 11:34:07 -0700

    Output all uptime checks

--- modules/uptime-checks/outputs.tf
+++ modules/uptime-checks/outputs.tf
@@ -1,4 +1,9 @@
-output "ids" {
-  description = "The IDs of the uptime checks."
-  value       = google_monitoring_uptime_check_config.self[*].id
+#output "ids" {
+#  description = "The IDs of the uptime checks."
+#  value       = google_monitoring_uptime_check_config.self[*].id
+#}
+
+output "uptime_checks" {
+  description = "All uptime checks managed"
+  value       = google_monitoring_uptime_check_config.self
 }
