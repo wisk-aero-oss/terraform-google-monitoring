654545718a3d91aedb53e3b42b1f080afe17e62d
Steven Nemetz <snemetz@notablehealth.com>
2023-05-24 16:51:39 -0700

    Fix output name

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -4,7 +4,7 @@
 #google_logging_organization_sink.self.id
 #google_logging_organization_sink.self.writer_identity
 
-output "logging_bucket_config_id" {
+output "logging_bucket_config" {
   description = "Full config of the logging bucket config"
   value       = google_logging_project_bucket_config.self
 }
