59bd9892a208c565f90fdc325de47c9504b4a4bb
Steven Nemetz <snemetz@notablehealth.com>
2023-05-26 11:05:54 -0700

    Add scoped projects back in

--- modules/project-scope/outputs.tf
+++ modules/project-scope/outputs.tf
@@ -4,11 +4,10 @@ output "metrics_scope_project" {
   description = "Metrics scope project ID"
   value       = local.metrics_scope_project
 }
-# No id ?
-#output "metrics_scoped_projects" {
-#  description = "Metrics scoped projects. All projects that metrics are being collected from"
-#  value       = [for id in google_monitoring_monitored_project.self[*].id : regex("^.*/([^/]+)$)", id)]
-#}
+output "metrics_scoped_projects" {
+  description = "Metrics scoped projects. All projects that metrics are being collected from"
+  value       = google_monitoring_monitored_project.self[0][*].name
+}
 output "monitored_projects" {
   description = "Monitored projects - Full data for debugging"
   value       = google_monitoring_monitored_project.self
